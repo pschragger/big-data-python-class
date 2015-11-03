a67c6ce94a8327e1fdaab0ec0bf3f6de855072a3
Paul Schragger <pschragger@gmail.com>
2015-11-03 00:01:35 -0500

    Lecture 7 - linkanalysis

---
+++ Lectures/code/Scrapy-Samples-master/crawlspider/craigslist_sample/spiders/test2.py
@@ -0,0 +1,27 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from craigslist_sample.items import CraigslistSampleItem
+
+
+class MySpider(CrawlSpider):
+    name = "craigs"
+    allowed_domains = ["sfbay.craigslist.org"]
+    start_urls = ["http://sfbay.craigslist.org/search/npo"]
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//a[@class="button next"]',)), callback="parse_items", follow= True),
+    )
+
+    def parse_items(self, response):
+        hxs = HtmlXPathSelector(response)
+        titles = hxs.xpath('//span[@class="pl"]')
+        parenturl = response.request.url
+        items = []
+        for titles in titles:
+            item = CraigslistSampleItem()
+            item["title"] = titles.xpath("a/text()").extract()
+            item["link"] = titles.xpath("a/@href").extract()
+            item["parent"] = parenturl
+            items.append(item)
+        return(items)
---
+++ Lectures/code/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = ["http://www.dmoz.org/Computers/Programming/Languages/Python/Books/","http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
+
+    def parse(self, response):
+                  f.write(response.body)
